8e5950fd5db9543115cf22ff9885df3d18950fbb
Ulyanov Leonid <ulyanov.leo.v@gmail.com>
2024-11-19 22:31:09 +0300

    Deleted redundant ./ strings

--- task8.sh
+++ task8.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
-touch ./newfile.txt
-mkdir ./newfolder
-mv ./newfile.txt ./newfolder
-cd ./newfolder
+touch newfile.txt
+mkdir -p newfolder
+mv newfile.txt newfolder
+cd newfolder
 echo $(date) "modified" >> newfile.txt
